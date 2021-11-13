2b8b3dced9f15d10cfbffdc809a7b816e9e0b240
Carlos Usuga Martinez <klich84@gmail.com>
2021-11-13 06:17:36 -0500

    Change file configuration

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,18 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDdSIdien4ykna9gOf49HXE9MeRkXMr3M",
-  authDomain: "adminusers-6dd02.firebaseapp.com",
-  databaseURL: "https://adminusers-6dd02-default-rtdb.firebaseio.com",
-  projectId: "adminusers-6dd02",
-  storageBucket: "adminusers-6dd02.appspot.com",
-  messagingSenderId: "771467567643",
-  appId: "1:771467567643:web:f3685d62d267e5ce32cebe"
-};
+	apiKey: 'AIzaSyCDdSIdien4ykna9gOf49HXE9MeRkXMr3M',
+	authDomain: 'adminusers-6dd02.firebaseapp.com',
+	projectId: 'adminusers-6dd02',
+	storageBucket: 'adminusers-6dd02.appspot.com',
+	messagingSenderId: '771467567643',
+	appId: '1:771467567643:web:f3685d62d267e5ce32cebe',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Cada modulo o componente que requiera conectarse a firebase debera importar esta conexion y añadir los servicios que requiera
+const firebaseApp = initializeApp(firebaseConfig)
+
+export default firebaseApp
